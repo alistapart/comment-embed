873f5b1d3e57d9d4703858f40a58cdc8a05279bb
Wilto <mat@matmarquis.com>
2013-02-12 15:13:34 -0500

    Initial iframe resizing seems to work now.

--- demo/sample-endpoint.php
+++ demo/sample-endpoint.php
@@ -5,26 +5,6 @@
 	<link rel="stylesheet" href="../dist/comment-embed.min.css"> 
 </head>
 <body>
-	<h1><?php
-	$id = $_REQUEST[ "id"];
-
-	if( $id == 1 ):
-?>
-
-	<div class="ala-embedded-comment" id="comment">
-		<a href="#" class="commenter-avatar" target="_parent">
-			<img src="http://d.alistapart.com/_made/pix/authors/uploads/photo_126389_60_60_c1.jpg" class="commenter-avatar" alt="" width="60" height="60">
-		</a>
-		<a class="commenter-name" href="#" target="_parent">Tim Murtaugh</a>
-		<a class="comment-meta" href="http://alistapart.com/comments/a-list-apart-relaunches-new-features-new-design#333649" target="_parent">5:03 pm on January 25, 2013</a>
-		
-		<p>The “v5” redirect bug has been knocked down!</p>
-	</div>
-<?php endif; ?>
-
-<?php
-	if( $id == 2 ):
-?>
 	<div class="ala-embedded-comment" id="comment">
 		<a href="#" class="commenter-avatar" target="_parent">
 			<img src="http://d.alistapart.com/_made/pix/authors/uploads/photo_116023_60_60_c1.jpg" width="60" height="60">
@@ -46,6 +26,5 @@
 
 		<p>I agree, and we plan to address select decisions in a series of brief blog posts, such as this one <a href="http://alistapart.com/blog/post/on-alt-text">on alt text</a>.</p>
 	</div>
-<?php endif; ?>
 </body>
 </html>
